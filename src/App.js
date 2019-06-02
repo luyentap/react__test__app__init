e54c3d887389dee15ce21deac7b95fe58b373f03
gaga201702 <bamboo2017002@gmail.com>
2019-06-02 21:26:37 +0700

    init webpack+babel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from "./Test";
 
 function App() {
   return (
@@ -18,7 +19,10 @@ function App() {
         >
           Learn React
         </a>
+        <p>edit this 1!!!</p>
       </header>
+      <Test/>
+
     </div>
   );
 }
---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+const path = require('path') // lấy đường dẫn tuyệt đối của thư mục
+console.log("path of @@: ");
+const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const webpack = require('webpack'); //to access built-in plugins
+
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    filename: '[name].bundle.js', //Code splitting thay cho bundel.js
+    path: path.resolve(__dirname, 'build')
+  },
+  module: {
+    rules: [
+        //loader các presets,.. vào webpack
+        //Biến đổi ES6 -> ES5 (nhưng chúng ta đã biết có 1 số trình duyệt không hỗ trợ es6, vì vậy cần thằng này để mình có thể sử dụng được trên tất cả các trình duyệt khác nhau)
+        // Webpack giúp kết nối các module lại với nhau:
+        // balel-loader kết nối babel vs webpack
+        // babel-core lấy và phân tích mã, output ra 1 số file
+        // babel-preset-env đảm nhận phần gọi biến đổi es6 -> es5
+        // babel-preset-react đản nhận phần biến đổi jsx -> js (Vì mình làm việc vs react nên cần mấy cái chuyển đổi này nhé ^^)
+      {
+        use: 
+            {   
+                loader:'babel-loader',
+                //có thể truyền vào từ config webpack/ babel.config.js
+                // options: {
+                //     presets: ['@babel/preset-env',"@babel/preset-react"]
+                // }
+        },
+        exclude: /node_modules/,
+        test: /\.js$/,
+        
+      },
+      //load file hình ảnh/font/...
+      {
+        test: /\.(png|jpe?g|gif|woff(2)?|ttf|eot|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {},
+          },
+        ],
+      },
+      
+      //style loader
+        //Mấy cái loader này sẽ được thực hiện theo thứ tự
+        // sass-loader transforms Sass thành CSS.
+        // css-loader parses CSS vào JavaScript và resolve dependencies.
+        // style-loader chèn CSS vào bên trong thẻ <style>
+        // Có thể hình dung nó như một function với callback là một function khác
+        // styleLoader(cssLoader(sassLoader("source")))
+            {
+            test:/\.css$/,
+            use:['style-loader','css-loader']
+        }
+    ]
+  },
+  plugins: [
+    new webpack.ProgressPlugin(),
+    //inject bundel vào index.html, dùng bản 3.0.7 fix lỗi undentified nếu xảy ra(linux,node 10)
+    new HtmlWebpackPlugin({template: __dirname+'/public/index.html'}),
+
+    //reload lại phần nào thay đổi trong html
+    new webpack.HotModuleReplacementPlugin(),
+    //nếu lỗi, ko tải lại trang mà chỉ console log
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+
+  //config for devServer
+  //fix for c9.io : check host:Invalid Host header
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+
+    //hot reload
+    port: 8080,
+    hot: true,
+    inline: true,
+    publicPath: '/',
+  }
+}
+module.exports = config;
